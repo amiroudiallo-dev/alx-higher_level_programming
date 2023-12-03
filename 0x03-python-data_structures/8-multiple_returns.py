0d617e128200a0455692ce57e37f30e7158ff778
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-12-03 00:31:40 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,6 @@
 # 8-multiple_returns.py
 # DIALLO Amirou Yannick Wendpuire <mailto:amroudiallo.yw@gmail.com>
 
-
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
     if sentence == "":
