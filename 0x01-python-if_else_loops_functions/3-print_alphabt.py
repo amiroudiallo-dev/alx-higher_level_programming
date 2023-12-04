01c0e52fa98b77720b14554dcbccc0413e4cf2a6
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-12-04 01:20:45 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for a in "abcdefghijklmnopqrstuvwxyz":
-    if(a not in "qe"):
-        print("{}".format(a), end='')
+for c in range(ord('a'), ord('z')+1):
+    if c is not (ord('q')) and c is not (ord('e')):
+        print('{}'.format(chr(c)), end='')
