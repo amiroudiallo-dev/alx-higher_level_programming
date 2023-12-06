79efe9efbd7e824325f1acf4b90b42c359da58a7
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2023-12-06 04:06:55 -0800

    11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return(list(map(lambda x: x*number, my_list)))
+# 11-multiply_list_map.py
+# DIALLO Amirou Yannick Wendpuire<mailto:amiroudiallo.yw@gmail.com>
+
+
+def multiply_list_map(my_list=[], number=0):
+    return list(map(lambda x: x * number, my_list))
