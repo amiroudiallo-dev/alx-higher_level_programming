6b8a414af24b3ae950f2ee95f5e3b031805d140f
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2024-01-09 02:25:38 -0800

    update repo

--- 0x07-python-test_driven_development/0-add_integer.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-def add_integer(a, b=98):
-    """ return a + b """
-    if not isinstance(a, (int or float)):
-        raise TypeError('a must be integer')
-    if not isinstance(b, (int or float)):
-        raise TypeError('b must an integer')
-    return int(a) + int(b)
