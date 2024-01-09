8558a6772bd63b9f4dc9224c25e1ae163a961bfa
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2024-01-09 07:55:11 -0800

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """
-Module 1-write_file
+Module 3-write_file
 
 Contains function that writes to text file and returns num chars written
 """
 
+
 def write_file(filename="", text=""):
-    with open(filename, 'w', encoding='utf-8') as file:
-        return file.write(text)
+    """writes to text file and returns num chars written"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return(f.write(text))
