bba9575a26b7e136549e7a17a4d97ebac76786fb
amiroudiallo-dev <amiroudiallo.yw@gmail.com>
2024-01-09 02:35:32 -0800

    2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""matrix divider module"""
 def matrix_divided(matrix, div):
-    """divides a matrix all elements by divider
-    """
-    if(not isinstance(matrix,list) or not len(matrix) or
-       0 in [len(listx) if type(listx) is list else 0 for listx in matrix] or
+    if(not isinstance(matrix,list) or
        any(False in x for x in  [[isinstance(ele,(int,float)) for ele in row]
-       for row in matrix])):
+                                   for row in matrix]) or
+        0 in [len(listx) for listx in matrix]):
         raise TypeError(
             'matrix must be a matrix (list of lists) of integers/floats')
     if(len(set([len(listx) for listx in matrix])) > 1):
