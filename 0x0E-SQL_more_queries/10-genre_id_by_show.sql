0d811e5ccbe2f801243580ac90358d172ec3a6b0
amiroudiallo.yw <80524359+amiroudiallo-dev@users.noreply.github.com>
2024-02-14 20:06:28 +0000

    10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
--- https://s3.amazonaws.com/intranet-projects-files/holbertonschool-higher-level_programming+/274/hbtn_0d_tvshows.sql
+
 -- run this first to import a SQL dump -->
 --      echo "CREATE DATABASE hbtn_0d_tvshows;" | mysql -uroot -p
 --      curl [link] -s | mysql -uroot -p hbtn_0d_tvshows
