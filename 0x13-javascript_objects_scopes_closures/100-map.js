699786fdb97950ebc97da09c13334b4474ddae19
Amirou DIALLO <amiroudiallo.yw@gmail.com>
2024-03-13 06:23:04 -0700

    100-map.js

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
+const list = require('./100-data').list;
+
+const newList = list.map((x, index) => x * index);
+
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(newList);
