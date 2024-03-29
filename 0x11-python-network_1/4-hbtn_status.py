e2c6dab535158c67cca07453d40b544f75b98755
Amirou DIALLO <amiroudiallo.yw@gmail.com>
2024-03-28 17:23:59 -0700

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""
-fetch https://intranet.hbtn.io/status; display response
-"""
+# requests use
 import requests
 
+
 if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+    r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
